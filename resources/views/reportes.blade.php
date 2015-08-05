da47cb071d0a60ca4db166bb749f00050c7c3a8b
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2015-08-05 14:10:02 -0500

    Changes to reports

--- resources/views/reportes.blade.php
+++ resources/views/reportes.blade.php
@@ -7,13 +7,7 @@
 	@section('content')
 		<h1 id="registro"> Reporte </h1>
 		{!!Form::open(array('url'=>'/reportes'))!!}
-			{!!Form::label('date1','Ingresa la fecha inicial:')!!}
-			{!! Form::input('date1', 'dob', date('d-m-Y'), ['class' => 'form-control', 'id' => 'dob', 'data-date-format' => "dd-mm-yyyy"]) !!}
-			</br>
-			{!!Form::label('date2','Ingresa la fecha final:')!!}
-			{!! Form::input('date2', 'dob', date('d-m-Y'), ['class' => 'form-control', 'id' => 'dob', 'data-date-format' => "dd-mm-yyyy"]) !!}
-			</br>
-			{!!Form::label('usuario','Usuarios:')!!}
+			{!!Form::label('usuario','Selecciona el Usuario:')!!}
             {!!Form::select('usuario', ['all'=>'Todos'] + $users, ['class' => 'form-control'])!!}
         	</br></br>
         	{!!Form::submit('Crear Reporte', ['class'=> 'btn btn-info form-control'])!!}
